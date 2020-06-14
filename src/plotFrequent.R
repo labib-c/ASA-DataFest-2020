fbd7852cd2f4a2784b652aac9d41f4720dd59c5b
labib <labibc01@gmail.com>
2020-06-14 11:25:59 -0400

    simple plot of frequent terms

---
+++ src/plotFrequent.R
@@ -0,0 +1,12 @@
+library(data.table)
+library(ggplot2)
+terms <- fread('/Users/labibchowdhury/Projects/ASA-DataFest-2020/covid-tweets/frequent_terms.csv')
+sorted_terms <- terms[order(terms$count, decreasing = TRUE),]
+p<-ggplot(data=sorted_terms[1:50], aes(reorder(term, count, sum), count)) +
+  geom_bar(stat="identity", width=0.5,position=position_dodge(0.6)) +
+  labs(y= "Count", x = "Terms") +
+  theme_minimal()
+p
+
+# Horizontal bar plot
+p + coord_flip()
