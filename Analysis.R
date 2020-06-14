d72c7843599346b0695ce19c87c5c8c85e202a6c
Andrew Chew <andy.chew@mail.utoronto.ca>
2020-06-14 08:34:11 -0400

    Added code to see total daily cases by day.

--- Analysis.R
+++ Analysis.R
@@ -3,7 +3,15 @@
 ## Authors:
 ## Date: June 14, 2020
 
+library(dplyr)
+library(ggplot2)
 library(utils)
 
 # Load COVID-19 daily cases data.
 data <- read.csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", na.strings = "", fileEncoding = "UTF-8-BOM")
+
+# Sum daily cases for all countries.
+d <- data.frame(date=data$dateRep, dailycount=data$cases)
+df <- d %>% group_by(date) %>% summarize(dailycount = sum(dailycount))
+
+plot(df$dailycount ~ df$date)
