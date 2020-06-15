84b6425ae86d4c13e5222d7bffd8a501e4bc726b
labib <labibc01@gmail.com>
2020-06-14 20:07:55 -0400

    made graphs more readable

--- src/aggregateCounts.R
+++ src/aggregateCounts.R
@@ -1,13 +1,15 @@
 library(data.table)
 library(ggplot2)
 library(dplyr)
+library(scales)
 hashtags_path <- "/Users/labibchowdhury/Projects/ASA-DataFest-2020/covid-tweets/hashtags"
 files <- list.files(path=hashtags_path, pattern="*.tsv", full.names=TRUE, recursive=FALSE)
 temp <- lapply(files, fread, sep="\t") 
 data <- rbindlist( temp ) %>% group_by(V1)
 data <- aggregate(data['V2'], by=data['V1'], sum)
 sorted <- data[order(data$V2, decreasing = TRUE),]
-p<-ggplot(data=sorted[1:50,], aes(reorder(V1, V2, sum), V2)) +
+options(scipen=7)
+p<-ggplot(data=sorted[1:20,], aes(reorder(V1, V2, sum), V2)) +
   geom_bar(stat="identity", width=0.5,position=position_dodge(0.6)) +
   labs(y= "Count", x = "Hashtag") +
   theme_minimal()
--- src/plotFrequent.R
+++ src/plotFrequent.R
@@ -2,7 +2,8 @@ library(data.table)
 library(ggplot2)
 terms <- fread('/Users/labibchowdhury/Projects/ASA-DataFest-2020/covid-tweets/frequent_terms.csv')
 sorted_terms <- terms[order(terms$count, decreasing = TRUE),]
-p<-ggplot(data=sorted_terms[1:50], aes(reorder(term, count, sum), count)) +
+options(scipen=7)
+p<-ggplot(data=sorted_terms[1:20], aes(reorder(term, count, sum), count)) +
   geom_bar(stat="identity", width=0.5,position=position_dodge(0.6)) +
   labs(y= "Count", x = "Terms") +
   theme_minimal()
